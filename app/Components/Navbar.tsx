66592b56293e29adaccf4a70fc8c1cab0001f1eb
AmitSharma <Sandy31524@gmail.com>
2025-11-23 22:24:02 +0530

    Added icons

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link"
 import { Navitems } from "../data/data"
-import { BsTwitter } from 'react-icons/bs'
+import { BsTwitter, BsTwitterX, BsX } from 'react-icons/bs'
+import { GiMailbox } from "react-icons/gi"
+import { SiGmail } from "react-icons/si"
+import { TbCircleHalf2 } from "react-icons/tb"
 export const Header = () => {
     return(
         <div className="flex items-center justify-between px-5 py-3">
@@ -15,7 +18,9 @@ export const Header = () => {
             </div>
             <div>
                 <ul className="flex gap-4">
-                    
+                    <li><BsTwitterX/></li>
+                    <li><SiGmail/></li>
+                    <li><TbCircleHalf2/></li>
                 </ul>
             </div>
         </div>
