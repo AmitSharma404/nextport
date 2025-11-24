43ca583f422db5c7bf773f0f1486d83736cca6fb
AmitSharma <Sandy31524@gmail.com>
2025-11-24 09:13:19 +0530

    ...

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const Card = () => {
     return(
         <div className="top-20 left-2 rounded-lg p-4 bg-foreground/5 sm:fixed lg:block hidden">
-            <div className='size-40 overflow-hidden rounded-full'>
+            <div className='size-40 overflow-hidden rounded-full bg-neutral-900'>
                 <Image src={nextsvd} alt="" className='size-auto overflow-x-hidden ' />
             </div>
             <p className='font-bold text-sm'>Want a Developer!</p>
